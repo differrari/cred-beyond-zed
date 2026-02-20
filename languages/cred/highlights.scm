83e6ef3a07a22a5d69e0ce19e4931e214dd18ad9
di <git@diferrari.com>
2026-02-20 00:00:00 +0000

    [CRED] comments & full identifiers

---
+++ languages/cred/highlights.scm
@@ -0,0 +1,9 @@
+(CONST) @text.literal
+(type) @type
+(func) @function
+(name) @variable
+(var) @variable
+["defer" "includeC" "include" "if" "else" "goto" "not" "for" "while" "do" "enum" "struct" "return" "interface" "switch" "case" "="] @attribute
+[(SYMBOL) "&" "*"] @attribute
+(OPERATOR) @attribute
+(comment) @comment
